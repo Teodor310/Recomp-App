dcff8aec8bb692170868bfd30a4e6a9ae8691370
Teodor310 <wtf163@haw-hamburg.de>
2025-06-17 23:24:41 +0200

    Home_Screen_v1

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:recomp_ai/data/constants.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -25,38 +27,102 @@ class _HomePageState extends State<HomePage> {
     final displayPercentage = (percentage * 100).clamp(0, 100).round();
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+    print("Screen height: $screenHeight");
 
-    return Scaffold(
-      body: Padding(
+    return SingleChildScrollView(
+      child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("This is Home page"),
-            const SizedBox(height: 20),
-            CustomPaint(
-              size: const Size(200, 200),
-              painter: CircleProgressPainter(percentage: percentage * 100),
-              child: SizedBox(
-                width: 200,
-                height: 200,
-                child: Center(
-                  child: Text(
-                    '$displayPercentage %',
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              CustomPaint(
+                size: const Size(200, 200),
+                painter: CircleProgressPainter(percentage: percentage * 100),
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  width: 200,
+                  height: 200,
+                  child: Center(
+                    child: Text(
+                      '$displayPercentage %',
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: changePercentage,
-              child: const Text("edit circle"),
-            ),
-          ],
+              const SizedBox(height: 50),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 3, bottom: 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Nutrients Indicator",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                color: widgitsColor,
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text('x/250', style: TextStyle(fontSize: 15)),
+                      LinearProgressIndicator(
+                        value: percentage,
+                        color: Color(0xFFE56571),
+                        minHeight: 7,
+                        borderRadius: BorderRadius.circular(60.0),
+                      ),
+                      Text("Proteins", style: TextStyle(fontSize: 17)),
+                      SizedBox(height: 20),
+                      Text('x/340', style: TextStyle(fontSize: 15)),
+                      LinearProgressIndicator(
+                        value: percentage,
+                        color: Color(0xFF59FFC4),
+                        minHeight: 7,
+                        borderRadius: BorderRadius.circular(60.0),
+                      ),
+                      Text("Carbs", style: TextStyle(fontSize: 17)),
+                      SizedBox(height: 20),
+                      Text('x/118', style: TextStyle(fontSize: 15)),
+                      LinearProgressIndicator(
+                        value: percentage,
+                        color: Color(0xFFFFA072),
+                        minHeight: 7,
+                        borderRadius: BorderRadius.circular(60.0),
+                      ),
+
+                      Text("Fats", style: TextStyle(fontSize: 17)),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: changePercentage,
+                    child: const Text("edit progress"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -70,8 +136,9 @@ class CircleProgressPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final strokeWidth = 20.0;
-    final radius = (size.width / 2) - strokeWidth / 2;
+    final strokeWidth = 25.0;
+    // final radius = (size.width / 2) - strokeWidth / 2;
+    final radius = size.width * 0.6;
     final center = Offset(size.width / 2, size.height / 2);
     final startAngle = -97.5 * pi / 180;
     final sweepAngle = 2 * pi * (percentage / 100);
